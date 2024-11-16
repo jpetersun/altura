6fffd3469a8e3a5ab92a870d2bc58dbf790fd023
petersonj020 <petersonj020@gmail.com>
2024-11-15 19:58:55 -0800

    test: add render user details test

--- src/test/app.spec.tsx
+++ src/test/app.spec.tsx
@@ -1,4 +1,4 @@
-import { expect, it, test } from "vitest";
+import { expect, test } from "vitest";
 import { render } from "vitest-browser-react";
 import App from "../App";
 import { fetchUsers } from "../api";
@@ -13,4 +13,18 @@ test("renders all users first names", async () => {
       return expect.element(getByText(user.firstName)).toBeInTheDocument();
     })
   );
+});
+
+test("renders user details when clicking on name", async () => {
+  const usersResult = fetchUsers();
+  const user = usersResult.users[0];
+  const { id, firstName, phoneNumber, username } = user;
+
+  const { getByText } = render(<App />);
+
+  await getByText(firstName).click();
+
+  await expect.element(getByText(String(id))).toBeInTheDocument();
+  await expect.element(getByText(phoneNumber)).toBeInTheDocument();
+  await expect.element(getByText(username)).toBeInTheDocument();
 });
